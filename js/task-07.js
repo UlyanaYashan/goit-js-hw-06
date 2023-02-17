5554849a50e465adedd88c92db824ba9196576f3
UlyanaYashan <ulyanalyala@gmail.com>
2023-02-17 09:21:04 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,12 @@
 
 const inputEl = document.querySelector('#font-size-control');
 const textSpan = document.querySelector('#text');
-
+textSpan.style.fontSize = inputEl.value + "px";
 
 inputEl.addEventListener('input', onInput);
-function onInput() {
-//     textSpan.style.fontSize = event.currentTarget.value + "px";
-    textSpan.style.fontSize = inputEl.value + "px";
+function onInput(event) {
+    textSpan.style.fontSize = event.currentTarget.value + "px";
+    
     
 }
 
